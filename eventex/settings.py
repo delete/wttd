9639a3a6ecfc47839e6e3a5991fec957e9adf881
delete <pinheiro.llip@gmail.com>
2013-09-15 23:22:54 -0300

    Subscription

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,17 +17,17 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #         default='sqlite3:///' + PROJECT_DIR.child('database.db'))
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(
+            default='sqlite3:///' + PROJECT_DIR.child('database.db'))
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
+    #     # The following settings are not used with sqlite3:
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #     'PORT': '',                      # Set to empty string for default.
+    # }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
