aff66e2a6a44e4116cbb41e99ba0c1671e67b2a3
delete <pinheiro.llip@gmail.com>
2013-09-15 21:47:36 -0300

    form test

---
+++ eventex/subscriptions/tests/test_models.py
@@ -0,0 +1,46 @@
+#coding: utf-8
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+from datetime import datetime
+from django.db import IntegrityError
+
+class SubscriptionsTest(TestCase):
+	def setUp(self):
+		self.obj = Subscription(
+			name='Fellipe Pinheiro',
+			cpf='12345678901',
+			email='pinheiro.llip@gmail.com',
+			phone='21-00000000'
+		)
+
+	def test_create(self):
+		'Subscription must have name, cpf, email, phone'
+		self.obj.save()
+		self.assertEqual(1, self.obj.id)
+
+	def test_has_created_at(self):
+		'Subscription must have automatic created_at'
+		self.obj.save()
+		self.assertIsInstance(self.obj.created_at, datetime)
+
+	def test_unicode(self):
+		self.assertEqual(u'Fellipe Pinheiro', unicode(self.obj))
+		
+
+class SubscriptionUniqueTest(TestCase):
+	def setUp(self):
+		'Create a firt entry to force the colision'
+		Subscription.objects.create(name='Fellipe Pinheiro', cpf='12345678901',
+						 email='pinheiro.llip@gmail.com', phone='21-00000000')
+
+	def test_cpf_unique(self):
+		'CPF must be unique'
+		s = Subscription(name='Fellipe Pinheiro', cpf='12345678901',
+						 email='outro@gmail.com', phone='21-00000000')
+		self.assertRaises(IntegrityError, s.save)
+
+	def test_email_unique(self):
+		s = Subscription(name='Fellipe Pinheiro', cpf='00000000011',
+						 email='pinheiro.llip@gmail.com', phone='21-00000000')
+		self.assertRaises(IntegrityError, s.save)
